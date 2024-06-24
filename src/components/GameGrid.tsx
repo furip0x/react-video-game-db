a0a1ed20878ff9863ff03082a84e34e3e5bb999c
furip0x <furkanipekci@hotmail.com>
2024-06-24 14:08:34 +0300

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,10 @@ const GameGrid = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
+        spacing={{
+          base: 4,
+          lg: 5,
+        }}
         padding="10px"
       >
         {isLoading &&
