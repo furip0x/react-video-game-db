82ffde0d6dd77d9dafdb96d00c76bbe1143bfbb2
furip0x <furkanipekci@hotmail.com>
2024-06-27 19:12:53 +0300

    Change inifinite scroll loader text to spinner

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react"
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import React from "react"
 import InfiniteScroll from "react-infinite-scroll-component"
 import { IGameQuery } from "../App"
@@ -26,7 +26,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       next={() => fetchNextPage()}
       hasMore={hasNextPage}
       style={{ width: "100%" }}
-      loader={<h4>Loading...</h4>}
+      loader={<Spinner />}
     >
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
