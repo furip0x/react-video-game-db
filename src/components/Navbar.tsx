e356bf9bf0e07ece9b2d17253d1ec44fb2f32442
furip0x <furkanipekci@hotmail.com>
2024-06-29 21:42:26 +0300

    Fix image size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div>
       <HStack padding="10px">
         <Link to="/">
-          <Image src={logo} boxSize="60px" />
+          <Image src={logo} boxSize="60px" objectFit="cover" />
         </Link>
         <SearchInput />
         <ColorModeSwitch />
